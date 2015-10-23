310ade664a84d75748a4b782915b8614d1104af4
Fatir Ahmad <djfdat@gmail.com>
2015-10-22 23:20:14 -0700

    Switched to using a log Added some more data to the xhr for the reqListener to use Simple error checking Bunch of TODO notes

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,6 +1,9 @@
 const url_usps = 'https://tools.usps.com/go/TrackConfirmAction.action?tLabels=';
-const response_element = document.getElementById('response_element')
 
+const log = document.getElementById('log')
+
+// TODO: Add dropdown selector for source
+const frm_add_tracking_number = document.getElementById('frm_add_tracking_number')
 const txt_add_tracking_number = document.getElementById('txt_add_tracking_number')
 const btn_add_tracking_number = document.getElementById('btn_add_tracking_number')
 
@@ -8,27 +11,47 @@ btn_add_tracking_number.addEventListener('click', add_tracking_number);
 
 function main() {
 
+	// TODO: Inital startup check looping through tracking_numbers
 }
 
 function add_tracking_number() {
-	check_shipping_status('usps', txt_add_tracking_number.value);
+
+	var tracking_number = txt_add_tracking_number.value;
+
+	// TODO: Actual validation per source
+	if (isNaN(tracking_number))
+	{
+		alert("Attempted to add invalid tracking_number: " + tracking_number)
+	}
+	else
+	{
+		alert("Adding tracking_number to tracking_numbers: " + tracking_number)
+		// TODO: Add to tracking_numbers
+
+		check_shipping_status('usps', tracking_number);
+	}
+	frm_add_tracking_number.reset();
 }
 
 function check_shipping_status(source, tracking_number) {
 
+	alert("Checking shipping status on tracking_number: " + tracking_number + " from source: " + source)
+
 	var xhr = new XMLHttpRequest();
 	xhr.addEventListener('load', reqListener);
+	xhr.tracking_number = tracking_number;
 
+	// TODO: Add logic for handling other sources
 	if (source == 'usps') {
 		xhr.open('GET', url_usps + tracking_number, true);
 		xhr.send();
 	}
 }
 
-function reqListener () {
+function reqListener (xhr) {
   var $sel = $(this.responseText);
   var status = artoo.scrape($sel.find('div.tracking-progress > div.progress-indicator > h2'), 'text')[0];
-  response_element.innerHTML = status;
+  log.innerHTML += xhr.target.tracking_number + "\t" + status + "<br/>";
 }
 
 main();
