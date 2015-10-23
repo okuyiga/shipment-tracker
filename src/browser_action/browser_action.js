cc752f9be6c893c1059b558e1e81fafaa9c00335
Fatir Ahmad <djfdat@gmail.com>
2015-10-22 22:44:55 -0700

    First pass on adding form for adding tracking slots

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,29 +1,33 @@
-const url_usps = "https://tools.usps.com/go/TrackConfirmAction.action?tLabels=";
+const url_usps = 'https://tools.usps.com/go/TrackConfirmAction.action?tLabels=';
+const response_element = document.getElementById('response_element')
 
-var response_element = document.getElementById("response_element")
+const txt_add_tracking_number = document.getElementById('txt_add_tracking_number')
+const btn_add_tracking_number = document.getElementById('btn_add_tracking_number')
+
+btn_add_tracking_number.addEventListener('click', add_tracking_number);
 
 function main() {
-	var xhr = new XMLHttpRequest();
-	xhr.addEventListener("load", reqListener);
-	xhr.open("GET", url_usps + "", true);
-	xhr.send();
+
+}
+
+function add_tracking_number() {
+	check_shipping_status('usps', txt_add_tracking_number.value);
 }
 
 function check_shipping_status(source, tracking_number) {
 
 	var xhr = new XMLHttpRequest();
-	xhr.addEventListener("load", reqListener);
+	xhr.addEventListener('load', reqListener);
 
-	if (source == "usps") {
-		xhr.open("GET", url_usps + tracking_number, true);
+	if (source == 'usps') {
+		xhr.open('GET', url_usps + tracking_number, true);
 		xhr.send();
 	}
 }
 
 function reqListener () {
   var $sel = $(this.responseText);
-  var status = artoo.scrape($sel.find("div.tracking-progress > div.progress-indicator > h2"), 'text')[0];
-  console.log(response_element)
+  var status = artoo.scrape($sel.find('div.tracking-progress > div.progress-indicator > h2'), 'text')[0];
   response_element.innerHTML = status;
 }
 
